457c95ed8ccb394d8ae0693363641f05c765cf09
victoriafomina <viky.fomina@yandex.ru>
2021-04-09 21:12:58 +0300

    Created a project. Lexer does not work correctly

---
+++ TextEditor/TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Windows;
+using Antlr4.Runtime;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestTheDitch();
+        }
+
+        public void TestTheDitch()
+        {
+            ICharStream stream = new AntlrFileStream("..\\..\\text.html");
+
+            // 
+            ITokenSource lexer = new HTMLLexer(stream);
+            // ITokenStream tokens = new CommonTokenStream(lexer);
+        }
+
+    }
+}
